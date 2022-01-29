a0dc930acfa5699d6ca613ed3dee440116a0b9d1
bwkst <s.t.kristine.or@gmail.com>
2022-01-29 22:28:08 +0800

    first
    初始

---
+++ pages/index/index.js
@@ -0,0 +1,90 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  environment_data: function(){
+    wx.navigateTo({
+      url: '/pages/environment_data/environment_data',
+      })
+  },
+
+  systematic_management: function(){
+    wx.navigateTo({
+      url: '/pages/systematic_management/systematic_management',
+      })
+  },
+
+  contact_employee: function(){
+    wx.navigateTo({
+      url: '/pages/contact_employee/contact_employee',
+      })
+  },
+
+  urgent_management: function(){
+    wx.navigateTo({
+      url: '/pages/urgent_management/urgent_management',
+      })
+  },
+})
