28fe40cb93abc94161073511a1d8b4d854f3d823
bwkst <s.t.kristine.or@gmail.com>
2022-06-28 23:38:06 +0800

    个人中心上半部分

--- pages/communication_center/area_of_member/area_of_member.js
+++ pages/communication_center/area_of_member/area_of_member.js
@@ -1,85 +1,36 @@
-// pages/communication_center/area_of_member/area_of_member.js
-const app = getApp()
 Page({
-  //显示用户头像+名字
-  
-  /**
-   * 页面的初始数据
-   */
   data: {
+    iconURL: '',
     nickName: '',
-    avatar: '',
-    openid: ''
   },
 
   //获取到用户的头像、昵称
   onLoad(options) {
     this.setData({
-      nickName: app.globalData.userInfo.nickName,
-      avatar: app.globalData.userInfo.avatarUrl
+      nickName: getApp().globalData.userInfo.nickName,
+      iconURL: getApp().globalData.userInfo.avatarUrl
     })
     wx.setStorageSync('Haslogin', true)
   },
 
   //退出登录并记录状态
   logout() {
-    app.globalData.userInfo = null
-    this.setData({
-      nickName: '',
-      avatar: ''
-    })
+    getApp().globalData.userInfo = null
     wx.setStorageSync('Haslogin', false)
     wx.redirectTo({
       url: '../login/login',
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  forumCenterPage: function () {
+    wx.redirectTo({
+      url: '../forum_center/forum_center',
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  postForumPage: function () {
+    wx.redirectTo({
+      url: '../post_forum/post_forum',
+    })
   }
 })
